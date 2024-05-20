fbadff3651af3fc100f71fa366d1dd0bdf4aa82b
mr.valerio <valera.osadchuk@gmail.com>
2024-05-20 20:05:40 +0300

    layout improvements

---
+++ components/PageHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <header class="text-center">
+    <h1 class="text-2xl font-black text-purple-900 text-center">{{ title }}</h1>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-purple-50 dark:bg-purple-900 flex flex-col w-full min-h-screen"
+  >
+    <slot />
+  </div>
+</template>
