a32a60d4f02c85599efb2419e8396c04900034af
mr.valerio <valera.osadchuk@gmail.com>
2024-05-20 12:58:47 +0300

    scaffold project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <CldImage src="cld-sample-4" width="640" height="480px" alt="an example image" />
+</template>
+
+<script setup lang="ts">
+const msg = ref("Hello World");
+</script>
+
+<style lang="scss">
+</style>
