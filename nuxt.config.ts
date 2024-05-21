c3991dbdf3b69d63e98f10a8e11ae11eaf4044f5
mr.valerio <valera.osadchuk@gmail.com>
2024-05-21 16:59:40 +0300

    init Vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/cloudinary", "@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  modules: [
+    "@nuxtjs/cloudinary",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    "@nuxt/test-utils/module",
+  ],
   cloudinary: {
     cloudName: "dr2lnj2sw",
   },
